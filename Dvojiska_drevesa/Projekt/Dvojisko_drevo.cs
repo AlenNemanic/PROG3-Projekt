8582a9b1ee068dd9c6fef4172763cdc6c20935ed
AlenNemanic <an9437@student.uni-lj.si>
2024-05-31 22:25:05 +0200

    Naredil osnovni razred

---
+++ Dvojiska_drevesa/Projekt/Dvojisko_drevo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Projekt
+{
+    public class Dvojisko_drevo<T> where T : IComparable<T>
+    {
+        public Dvojisko_drevo(T vrednost)
+        {
+            Vrednost = vrednost;
+            Levo = null;
+            Desno = null;
+        }
+
+        public T Vrednost { get; set; }
+        public Dvojisko_drevo<T> Levo { get; set; }
+        public Dvojisko_drevo<T> Desno { get; set; }
+
+        public bool Iskanje(T vrednost)
+        {
+            if (Vrednost == null)
+                return false;
+            if (Vrednost.Equals(vrednost))
+                return true;
+            if (Levo != null && Levo.Iskanje(vrednost))
+                return true;
+            if (Desno != null && Desno.Iskanje(vrednost))
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Dvojiska_drevesa/Projekt/Iskalno_dvojisko_drevo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Projekt
+{
+    public class Iskalno_dvojisko_drevo<T> : Dvojisko_drevo<T> where T : IComparable<T>
+    {
+        public Iskalno_dvojisko_drevo(T vrednost) : base(vrednost)
+        {
+        }
+    }
+}
