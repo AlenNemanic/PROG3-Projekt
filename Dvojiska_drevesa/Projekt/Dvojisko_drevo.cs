7da4b6533da531fc939b232f52b3937b0ebf0f19
AlenNemanic <an9437@student.uni-lj.si>
2024-06-06 13:13:02 +0200

    Popravil razred Dvojisko_drevo

--- Dvojiska_drevesa/Projekt/Dvojisko_drevo.cs
+++ Dvojiska_drevesa/Projekt/Dvojisko_drevo.cs
@@ -4,26 +4,60 @@ namespace Projekt
 {
     public class Dvojisko_drevo<T> where T : IComparable<T>
     {
-        public Dvojisko_drevo(T vrednost)
+        private T _podatek;
+        private Dvojisko_drevo<T> _levoPoddrevo;
+        private Dvojisko_drevo<T> _desnoPoddrevo;
+        private bool _prazno;
+
+        // Konstruktor
+        public Dvojisko_drevo(T podatek = default(T), Dvojisko_drevo<T> levo = null, Dvojisko_drevo<T> desno = null)
         {
-            Vrednost = vrednost;
-            Levo = null;
-            Desno = null;
+            _podatek = podatek;
+            _levoPoddrevo = levo;
+            _desnoPoddrevo = desno;
+            _prazno = false;
+            if (podatek == null && levo == null && desno == null)
+                _prazno = true;
         }
 
-        public T Vrednost { get; set; }
-        public Dvojisko_drevo<T> Levo { get; set; }
-        public Dvojisko_drevo<T> Desno { get; set; }
+        // Lastnost za pridobivanje in nastavljanje podatka v korenu
+        public T Podatek
+        {
+            get { return _podatek; }
+            set { _podatek = value; }
+        }
 
+        // Lastnost za pridobivanje in nastavljanje levega poddrevesa
+        public Dvojisko_drevo<T> Levo
+        {
+            get { return _levoPoddrevo; }
+            set { _levoPoddrevo = value; }
+        }
+
+        // Lastnost za pridobivanje in nastavljanje desnega poddrevesa
+        public Dvojisko_drevo<T> Desno
+        {
+            get { return _desnoPoddrevo; }
+            set { _desnoPoddrevo = value; }
+        }
+
+        // Lastnost za preverjanje ali je drevo prazno
+        public bool Prazno
+        {
+            get { return _prazno; }
+            private set { _prazno = value; }
+        }
+
+        // Metoda za iskanje vrednosti v drevesu
         public bool Iskanje(T vrednost)
         {
-            if (Vrednost == null)
+            if (Prazno)
                 return false;
-            if (Vrednost.Equals(vrednost))
+            if (Podatek.Equals(vrednost))
                 return true;
-            if (Levo != null && Levo.Iskanje(vrednost))
+            if (!Levo.Prazno && Levo.Iskanje(vrednost))
                 return true;
-            if (Desno != null && Desno.Iskanje(vrednost))
+            if (!Desno.Prazno && Desno.Iskanje(vrednost))
                 return true;
             return false;
         }
